ccdb8f89fdf7003611fcb7c766204246001cc101
Andrew Russo and Jeffrey Hackford <jeffrey@>
2016-05-25 15:36:11 -0700

    add prototype method getDino and build front end

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,28 +1,86 @@
 var apiKey = require('./../.env').apiKey;
 var dinoArray = [];
+var Dino = require('./../js/Dino.js').DinoModule;
 var fakeDinos = ["testasaurous", "fakeasaurous", "a", "b", "c"];
 
 $(document).ready(function(){
 
+var DinoTest = new Dino();
+console.log(DinoTest);
+
   $('#dino').submit(function(event) {
     event.preventDefault();
-    // debugger;
-    $.get(apiKey).then(function(response){
-      var dinos = response;
-      dinoArray.push(dinos);
-    });
-    $.get(apiKey).then(function(response){
-      var dinos = response;
-      dinoArray.push(dinos);
-    });
+    // $.get(apiKey).then(function(response){
+    //   var dinos = response;
+    //   dinoArray.push(dinos);
+    // });
+    // $.get(apiKey).then(function(response){
+    //   var dinos = response;
+    //   dinoArray.push(dinos);
+    // });
+    var Dino1 = DinoTest.getDino();
+    dinoArray.push(Dino1);
+
+    var Dino2 = DinoTest.getDino();
+    dinoArray.push(Dino2);
+
     var randomPick = Math.floor(Math.random() * fakeDinos.length);
     dinoArray.push(fakeDinos[randomPick]);
-
-    // var fakeDino = fakeDinos[Math.floor((Math.random() * fakeDinos.length) + 1)];
 
     console.log(dinoArray);
     dinoArray= [];
 
   });
-  // console.log(dinoArray);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var apiKey = require('./../.env').apiKey;
+// var dino = require('./../js/Dino.js').Dino;
+// var fakeDinos = ["testasaurous", "fakeasaurous", "a", "b", "c"];
+//
+// $(document).ready(function(){
+//   var dinoArray = [];
+//
+//   $('#dino').submit(function(event) {
+//     event.preventDefault();
+//
+//     var dino1;
+//     var dino2;
+//     var dino3;
+//
+//     $.get(apiKey).then(function(response){
+//       var dino1 = response;
+//     });
+//     $.get(apiKey).then(function(response){
+//       var dino2 = response;
+//     });
+//     var randomPick = Math.floor(Math.random() * fakeDinos.length);
+//     var dino3 = (fakeDinos[randomPick]);
+//
+//     var newDino = new Dino(dino1, dino2, dino3);
+//     newDino.shuffle();
+//     dinoArray.push(newDino);
+//     console.log(dinoArray);
+//     dinoArray= [];
+//   });
+// });
--- js/dino.js
+++ js/dino.js
@@ -1,3 +1,27 @@
-exports.Dino = function(){
+var apiKey = require('./../.env').apiKey;
 
+  exports.dinos;
+
+
+exports.DinoModule = function(){
+
+};
+exports.DinoModule.prototype.getDino = function() {
+  $.get(apiKey).then(function(response){
+    var dinos = response;
+    $('#row').append('<h1 class="jumbotron col-xs-4"><span id="dino1random">' + dinos + '</span></h1>');
+    console.log(dinos);
+  });
+
+};
+
+
+exports.DinoModule.prototype.shuffle = function(dinoArray) {
+    var j, x, i;
+    for (i = dinoArray.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = dinoArray[i - 1];
+        dinoArray[i - 1] = dinoArray[j];
+        dinoArray[j] = x;
+    }
 };
