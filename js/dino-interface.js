96d65042945748b99a4a2b652f7b3ec83c74d3c6
Andrew Russo and Jeffrey Hackford <andrew@>
2016-05-25 11:44:59 -0700

    add random function to pick a fake dino and push to array

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,21 +1,28 @@
 var apiKey = require('./../.env').apiKey;
+var dinoArray = [];
+var fakeDinos = ["testasaurous", "fakeasaurous", "a", "b", "c"];
 
 $(document).ready(function(){
 
   $('#dino').submit(function(event) {
     event.preventDefault();
+    // debugger;
     $.get(apiKey).then(function(response){
-      $('#dino1random').text(response);
-      console.log(response);
+      var dinos = response;
+      dinoArray.push(dinos);
     });
     $.get(apiKey).then(function(response){
-      $('#dino2random').text(response);
-      console.log(response);
+      var dinos = response;
+      dinoArray.push(dinos);
     });
-    $.get(apiKey).then(function(response){
-      $('#dino3random').text(response);
-      console.log(response);
-    });
+    var randomPick = Math.floor(Math.random() * fakeDinos.length);
+    dinoArray.push(fakeDinos[randomPick]);
+
+    // var fakeDino = fakeDinos[Math.floor((Math.random() * fakeDinos.length) + 1)];
+
+    console.log(dinoArray);
+    dinoArray= [];
 
   });
+  // console.log(dinoArray);
 });
